5775c9c5e26ed6f1b3dd0a84ba3cf1db624f1988
sairah814 <sairah814@gmail.com>
2016-04-06 00:37:07 -0400

    added all apis and started styling

--- js/app.js
+++ js/app.js
@@ -22,8 +22,8 @@ $(document).ready(function () {
         dataType: 'jsonp'
     })
 
-    .done(function (result) { //this waits for the ajax to return with a succesful promise object
-            console.log(result.products);
+    .done(function (result) {
+            //console.log(result.products);
             $.each(result.products, function (index, item) {
                 var essential = showStream(item);
             });
@@ -31,7 +31,7 @@ $(document).ready(function () {
             //if the response is failure
 
         })
-        .fail(function (jqXHR, error) { //this waits for the ajax to return with an error promise object
+        .fail(function (jqXHR, error) {
             $('.cameras').append(error);
             console.log(error);
         });
@@ -45,48 +45,115 @@ $(document).ready(function () {
 
     //GOOGLE BOOKS API
     var bookrequest = "Beginner Photography";
-    var url = 'https://www.googleapis.com/books/v1/volumes';
+    var booksurl = 'https://www.googleapis.com/books/v1/volumes';
     var bookparams = {
         key: 'AIzaSyAMQwLJB37WSjNqsk1gbZwhP4B7J9T5jwk',
         q: bookrequest,
-        maxResults: 20
+        maxResults: 9
     };
     $.ajax({
-            url: url,
+            url: booksurl,
             data: bookparams,
             dataType: "jsonp", //use jsonp to avoid cross origin issues
             type: "GET",
         })
-        .done(function (result) { //this waits for the ajax to return with a succesful promise object
-            //var searchResults = showSearchResults(request.tagged, result.items.length);
-            //$.each is a higher order function. It takes an array and a function as an argument.
-            //The function is executed once for each item in the array.
-            console.log(result);
+        .done(function (result) {
+            //console.log(result);
             $.each(result.items, function (i, item) {
                 var booklist = item.volumeInfo.title;
-                var bookresults = sortBooks(booklist);
-                console.log(booklist);
+                var bookresults = showBooks(booklist);
+                //console.log(booklist);
             });
         })
-        .fail(function (jqXHR, error) { //this waits for the ajax to return with an error promise object
+        .fail(function (jqXHR, error) {
             var errorElem = showError(error);
             $('.books').append(errorElem);
         });
-    var bookarray = [];
 
-    function sortBooks(booklist) {
-        bookarray.push(booklist);
-        if (bookarray.length == 19) {
-            console.log(bookarray);
-        }
-    }
 
     function showBooks(bookitem) {
         var bookname = bookitem;
         $('.books').append(bookname);
     }
 
-    /*filtering by brand:
-    $('.quiz-section').on('click', '.option', function () {
-    var answer = $("input[class='option']:checked").val();*/
+    //Flickr API
+    var flickrurl = 'https://api.flickr.com/services/rest/';
+    var photoparams = {
+        method: 'flickr.groups.pools.getPhotos',
+        api_key: '23d67b1c61e2e05e99bd91280fd1f0d6',
+        group_id: '1601790@N20',
+        extras: 'url_sq, views',
+        nojsoncallback: 1,
+        format: 'json',
+        per_page: 15
+
+    };
+    $.ajax({
+            url: flickrurl,
+            data: photoparams,
+            //dataType: 'jsonp',
+            type: 'GET'
+        })
+        .done(function (result) {
+            //console.log(result.photos.photo);
+            $.each(result.photos.photo, function (i, item) {
+                var photos = showPhotoIdeas(item);
+                $('.ideas-content').append(photos);
+            });
+
+        })
+        .fail(function (jqXHR, error) {
+            console.log(error);
+            $('.ideas-content').append(error);
+
+        });
+
+    function showPhotoIdeas(photostream) {
+        //var image = "<img src='" + product.image + "' alt='" + product.name + "'>";
+
+        var imageurl = photostream.url_sq;
+        var imagename = photostream.title;
+        var imageviews = parseInt(photostream.views);
+        //return ("<img src='" + imageurl + "' alt='" + imagename + "'>");
+        if (imageviews > 10) {
+            return ("<img src='" + imageurl + "' alt='" + imagename + "'>");
+        }
+    }
+
+    //YouTube API
+
+    var videoparams = {
+        key: 'AIzaSyAe0bg4MZazw0wK_e0Wpp8lPzW716Ih0zY',
+        q: 'Beginner Photography',
+        part: 'snippet',
+        type: 'video',
+        maxResults: 9
+    };
+    var youtubeurl = 'https://www.googleapis.com/youtube/v3/search';
+    $.ajax({
+            url: youtubeurl,
+            data: videoparams,
+            dataType: 'jsonp',
+            type: 'GET'
+        })
+        .done(function (result) {
+            //console.log(result);
+            $.each(result.items, function (i, item) {
+                showResults(item);
+            });
+
+        })
+        .fail(function (jqXHR, error) {
+            console.log(error);
+            $('.tutorials').append(error);
+
+        });
+
+    function showResults(results) {
+        //var output = "<p>" + results.snippet.channelTitle + "<img src='" + results.snippet.thumbnails.high.url + "'/></p>";
+        var videourl = results.id.videoId;
+        var output = "<iframe height='315' width='420' src='http://www.youtube.com/embed/" + videourl + "' frameborder='0' allowfullscreen></iframe>";
+        $('.tutorials').append(output);
+    }
+
 });
