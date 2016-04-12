247c0863ef8745307eebebef0adb0d031d9c5b76
sairah814 <sairah814@gmail.com>
2016-04-11 23:06:33 -0400

    deleted angular sample

--- js/app.js
+++ js/app.js
@@ -245,16 +245,4 @@ $(document).ready(function () {
             return ("<img class= 'materials' src='" + imageurl + "' alt='" + imagename + "'>");
         }
     }
-
-    phonecatApp.config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/phones', {
-            templateUrl: 'partials/phone-list.html',
-            controller: 'PhoneListCtrl'
-        }).when('/phones/:phoneId', {
-            templateUrl: 'partials/phone-detail.html',
-            controller: 'PhoneDetailCtrl'
-        }).otherwise({
-            redirectTo: '/phones'
-        });
-    }]);
 });
